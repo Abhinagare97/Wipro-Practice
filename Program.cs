685353b2ffff31f53f7e198d92c9a1a727e4cb11
Abhinagare97 <nagareabhishek.gss@gmail.com>
2025-02-05 10:08:56 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// using System;
+// namespace Practice1{
+
+//     struct Car{
+//         private string name;
+//         public string model;
+//         string price;
+//         Console.WriteLine("sd";
+        
+        
+        
+        
+        
+        
+        
+        
+//         )
+//     }
+//     public class Program{
+//         public static void Main(){
+//             Console.WriteLine("Hi lets get in.");
+//             Car c = new Car();
+//             c.name="Bugatti";
+//             c.model="Veyron";
+//             c.price="100000000";
+//             Console.WriteLine("Name: "+c.name);
+//             Console.WriteLine("Model: "+c.model);
+//             Console.WriteLine("Price: "+c.price);
+//         }
+//     }
+// }
